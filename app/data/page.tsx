0b978be95d3566aec8930e6d56f083c2ffb09128
HMaroba <marobahlalele910@gmail.com>
2024-10-23 08:44:30 +0200

    Removed black bg

--- app/data/page.tsx
+++ app/data/page.tsx
@@ -1,18 +1,17 @@
-
 type IPost = {
-    id: number;
-    title : string;
-    content: string;
-}
+  id: number;
+  title: string;
+  content: string;
+};
 
 export default async function Page() {
-    const data = await fetch('https://api.vercel.app/blog')
-    const posts = await data.json()
-    return (
-      <ul>
-        {posts.map((post : any) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
-    )
-  }
+  const data = await fetch("https://api.vercel.app/blog");
+  const posts = await data.json();
+  return (
+    <ul>
+      {posts.map((post: IPost) => (
+        <li key={post.id}>{post.title}</li>
+      ))}
+    </ul>
+  );
+}
