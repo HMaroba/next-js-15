99b58e15a21eb23d58b500abd0117891fa6d358d
HMaroba <marobahlalele910@gmail.com>
2024-11-02 10:04:24 +0200

    Created Register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,12 +2,24 @@
 
 export default function Register() {
   return (
-    <div className="p-2 flex justify-center items-center min-h-screen"> 
-      <input
-        className="p-2 border-2 border-gray-400 text-gray-600 rounded-md w-2/6 ring-0 focus:ring-0"
+    <div className="p-2 flex flex-col  justify-center items-center min-h-screen"> 
+ 
+  <input
+        className="p-2 border-2 border-gray-400 text-gray-600 rounded-md w-2/6 focus:outline outline-none"
         placeholder="Username"
         type="text"
       />
+            <input
+        className="p-2 border-2 border-gray-400 text-gray-600 rounded-md w-2/6 focus:outline outline-none"
+        placeholder="Username"
+        type="text"
+      />
+            <input
+        className="p-2 border-2 border-gray-400 text-gray-600 rounded-md w-2/6 focus:outline outline-none"
+        placeholder="Username"
+        type="text"
+      />
+
     </div>
   );
 }
