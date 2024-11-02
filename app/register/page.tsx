893b9e1a72c3b76e728d238a02d3bb959503644c
HMaroba <marobahlalele910@gmail.com>
2024-11-02 09:58:21 +0200

    Created Register

---
+++ app/register/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function Register() {
+  return (
+    <div className="p-2 flex justify-center items-center min-h-screen"> 
+      <input
+        className="p-2 border-2 border-gray-400 text-gray-600"
+        placeholder="Username"
+        type="text"
+      />
+    </div>
+  );
+}
