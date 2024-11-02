a2fca6a18f166a9765e33acf9a32b30dba64f9bf
HMaroba <marobahlalele910@gmail.com>
2024-11-02 10:01:37 +0200

    Created Register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,7 +4,7 @@ export default function Register() {
   return (
     <div className="p-2 flex justify-center items-center min-h-screen"> 
       <input
-        className="p-2 border-2 border-gray-400 text-gray-600"
+        className="p-2 border-2 border-gray-400 text-gray-600 rounded-md w-2/6 ring-0 focus:ring-0"
         placeholder="Username"
         type="text"
       />
